104cb30832c0a04ceefb2923c609ed4803568d62
Chetan Rasik <115062830+chetan-thakur654@users.noreply.github.com>
2024-03-02 12:46:12 +0530

    Update courierScraper.js

--- courierScraper.js
+++ courierScraper.js
@@ -105,71 +105,78 @@ const courierScrapers = {
 
   //   url: (trackingId) => `https://transitpl.com/track`,
   // },
-  "vrl-logistics-courier-tracking": {
+  "overseas-courier-tracking": {
     scrapeData: async (trackingId, page) => {
       // Construct the URL for tracking information
-      const url = `https://www.vrlgroup.in/track_consignment.aspx`;
+      const url = `http://www.overseaslogistics.in/tracking.aspx`;
 
       // Navigate to the tracking page and wait for it to load
-      await page.goto(url, { timeout: 60000, waitUntil: "load" });
-
-      //Add tracking Id to input box
-      await page.type("#lrno", trackingId);
-      await page.waitForTimeout(2000);
-
-      await page.click(
-        "#team > div > div:nth-child(2) > div:nth-child(2) > input"
-      );
-      await page.waitForTimeout(2000);
-
-      // await page.waitForNavigation();
-
-      await page.waitForSelector("#accordionExample", {
+      await page.goto(url, {
         timeout: 60000,
         waitUntil: "load",
       });
 
+      await page.waitForSelector(`#ContentPlaceHolder1_text`, {
+        timeout: 12000,
+        waitUntil: "load",
+      });
+
+      await page.type("#ContentPlaceHolder1_text", trackingId);
+      await page.waitForTimeout(2000);
+
+      await page.click("#ContentPlaceHolder1_Button1");
+
+      await page.waitForSelector(
+        `#ContentPlaceHolder1_DataList1_grdstate_0 > tbody > tr`,
+        {
+          timeout: 12000,
+          waitUntil: "load",
+        }
+      );
+
       // Extract tracking information using Puppeteer's evaluate function
-      const trackingInfo = await page.evaluate(() => {
+      const trackingInfo = await page.evaluate(async () => {
         // Extract delivery status
-        const deliveryStatus = document
-          .querySelector("#result_div > div:nth-child(1)")
+        const deliveryStatus = await document
+          .querySelector("#ContentPlaceHolder1_DataList1_Label7_0")
           .innerText.trim();
 
-        let from = document
-          .querySelector("#collapseBooking > div > div > div:nth-child(2)")
-          .innerText.trim()
-          .split("\n")[1];
-        let to = document
-          .querySelector("#collapseBooking > div > div > div:nth-child(3)")
-          .innerText.trim()
-          .split("\n")[1];
+        let to = await document
+          .querySelector("#ContentPlaceHolder1_DataList1_Label16_0")
+          .innerText.trim();
+
         // Extract checkpoints information
         const checkpoints = Array.from(
-          document.querySelectorAll(`#collapseTransit > div > div`)
-        ).map((checkpoint) => ({
-          date: checkpoint
-            .querySelector("div:nth-child(3)")
-            .innerText.split("\n")[1],
-          time: "",
-          activity: `${checkpoint
-            .querySelector("div:nth-child(1)")
-            .innerText.split("\n")
-            .join(" ")} ${checkpoint
-            .querySelector("div:nth-child(2)")
-            .innerText.split("\n")
-            .join(" ")}`,
-          courierName: "VRL Logistics",
-          location: "",
-        }));
+          document.querySelectorAll(
+            "#ContentPlaceHolder1_DataList1_grdstate_0 > tbody > tr"
+          )
+        )
+          .slice(1)
+          .map((checkpoint) => {
+            return {
+              date: checkpoint
+                .querySelector("td:nth-child(1)")
+                .innerText.trim(),
+              time: checkpoint
+                .querySelector("td:nth-child(2)")
+                .innerText.trim(),
+              activity: checkpoint
+                .querySelector("td:nth-child(4)")
+                .innerText.trim(),
+              courierName: "OverSeas Logistics",
+              location: checkpoint
+                .querySelector("td:nth-child(3)")
+                .innerText.trim(),
+            };
+          });
 
-        return { deliveryStatus, from, to, checkpoints };
+        return { deliveryStatus, to, checkpoints };
       });
 
       return trackingInfo;
     },
 
-    url: (trackingId) => `https://www.vrlgroup.in/track_consignment.aspx`,
+    url: (trackingId) => `http://www.overseaslogistics.in/tracking.aspx`,
   },
 };
 
